ad06222ec8fc5ec8349437ad614a3934950380ae
Jules <juliannewood2413@gmail.com>
2021-02-22 10:43:38 -0700

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
