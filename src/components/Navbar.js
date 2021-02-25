ef9cae86cc164c7433029b1c1aca9e5cdff40590
Jules <juliannewood2413@gmail.com>
2021-02-25 09:23:25 -0700

    updated contact page/links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,6 +35,7 @@ function Navbar() {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                    {/* <img src="./images/bitmoji.png"></img> */}
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
