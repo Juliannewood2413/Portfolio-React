8c51dae761a37db31c35bf993b9bead6b9c4e2b2
Jules <juliannewood2413@gmail.com>
2021-02-28 19:12:11 -0700

    deploy x3

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+require('dotenv').config()
 
 
 function App() {
   return (
     <>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
      <Navbar />
      <Switch>
        <Route path="/" exact component ={Home} />
